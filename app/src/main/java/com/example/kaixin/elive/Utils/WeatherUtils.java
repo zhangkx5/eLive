875ecaf1dc5ff40fe98316364c35afb0776eedc9
baoanj <baoanj@live.com>
2017-06-01 20:28:29 +0800

    完善日记模块,添加日记锁,界面更友好

--- app/src/main/java/com/example/kaixin/elive/Utils/WeatherUtils.java
+++ app/src/main/java/com/example/kaixin/elive/Utils/WeatherUtils.java
@@ -23,39 +23,7 @@ public class WeatherUtils {
 
     private static final String web = "http://ws.webxml.com.cn/WebServices/WeatherWS.asmx/getWeather";
 
-    private static final int UPDATE_CONTENT = 0;
-
-    public static ArrayList<String> list = new ArrayList<String>();
-
-    /*private static Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            if (msg.what == UPDATE_CONTENT) {
-                response = (ArrayList<String>)msg.obj;
-                /*String result = response.get(0);
-                if ("查询结果为空".equals(result)) {
-                    Toast.makeText(MainActivity.getAppContext(), "当前城市不存在，请重新输入", Toast.LENGTH_SHORT).show();
-                } else if ("发现错误：免费用户不能使用高速访问。http://www.webxml.com.cn/".equals(result)){
-                    Toast.makeText(MainActivity.getAppContext(), "您的点击速度过快，二次查询间隔<600ms", Toast.LENGTH_SHORT).show();
-                } else if ("发现错误：免费用户 24 小时内访问超过规定数量。http://www.webxml.com.cn/".equals(result)) {
-                    Toast.makeText(MainActivity.getAppContext(), "免费用户24小时内访问超过规定数量50次", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (response.size() > 28) {
-                        String where = response.get(1);
-                        name.setText(where);
-                        String timestr = response.get(3);
-                        time_update.setText(timestr.substring(11, timestr.length()) + " 更新");
-                        showWeather();
-                        showAir();
-                        degrees.setText(response.get(8));
-                        showList();
-                        showNextFiveDay();
-                    } else {
-                        Toast.makeText(MainActivity.getAppContext(), response.get(0), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        }
-    };*/
+    public static ArrayList<String> list = new ArrayList<>();
 
     public static ArrayList<String> postRequest() {
         HttpURLConnection connection = null;
@@ -93,6 +61,7 @@ public class WeatherUtils {
         }
         return list;
     }
+
     private static void parseXMLWithPull(String str_xml) {
         try {
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
